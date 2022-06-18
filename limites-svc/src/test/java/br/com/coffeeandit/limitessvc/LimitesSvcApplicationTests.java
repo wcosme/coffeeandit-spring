0d33807a6d292d5422ae7d4a1de95ca87b9d1568
Wallace Gonçalves <wallacecosme@gmail.com>
2022-06-18 12:19:30 -0300

    Setup inicial do projeto limites-svc

---
+++ limites-svc/src/test/java/br/com/coffeeandit/limitessvc/LimitesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coffeeandit.limitessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
